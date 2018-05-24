29e43188838b6c39302b8757f37342ece8b27341
Alexei Ersov <alexei@mixbook.com>
2018-05-24 16:56:35 +0300

    Code cleanup

--- task-02/ip_address.rb
+++ task-02/ip_address.rb
@@ -1,12 +1,10 @@
 class IpAddress
-    attr_reader :string_value
-
     def initialize string_value
         @string_value = string_value
     end
 
     def valid?
-        octets = string_value.split('.')
+        octets = @string_value.split('.')
 
         octets.length == 4 && octets.all? do |octet_str|
             octet = octet_str.to_i
