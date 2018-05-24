6fd0151d7cc62b5cd45c0bbc4bb080bffbdbe689
Alexei Ersov <alexei@mixbook.com>
2018-05-24 13:49:22 +0300

    Update task 1

--- task-01/duration_formatter.rb
+++ task-01/duration_formatter.rb
@@ -1,38 +1,43 @@
 class DurationFormatter
     class TimeSpan
-        attr_reader :name, :duration_in_seconds
-
-        def initialize(name, duration_in_seconds)
+        def initialize(name, duration_in_seconds, max_value)
             @name = name
             @duration_in_seconds = duration_in_seconds
+            @max_value = max_value
         end
 
         def format(seconds)
-            value = seconds / duration_in_seconds
+            value = seconds / @duration_in_seconds
+
+            value %= @max_value if @max_value
             
             if value == 0
                 nil
             else
-                "#{value} #{name}#{'s' if value > 1}"
+                "#{value} #{@name}#{'s' if value > 1}"
             end
+        end
+
+        def max_value_in_seconds
+            @max_value.nil? ? nil : @max_value * @duration_in_seconds
         end
     end
 
     def format_duration seconds
         return 'now' if seconds == 0
 
+        second = TimeSpan.new("second", 1, 60)
+        minute = TimeSpan.new("minute", second.max_value_in_seconds, 60)
+        hour = TimeSpan.new("hour", minute.max_value_in_seconds, 24)
+        day = TimeSpan.new("day", hour.max_value_in_seconds, 365)
+        year = TimeSpan.new("year", day.max_value_in_seconds, nil)
+
         spans = [
-            TimeSpan.new("year", 365 * 24 * 60 * 60),
-            TimeSpan.new("day", 24 * 60 * 60),
-            TimeSpan.new("hour", 60 * 60),
-            TimeSpan.new("minute", 60),
-            TimeSpan.new("second", 1)
+            year, day, hour, minute, second
         ]
 
         span_strings = spans.map do |s|
-            v = s.format(seconds)
-            seconds %= s.duration_in_seconds
-            v
+            s.format(seconds)
         end.reject(&:nil?)
 
         first_part = span_strings[0..-2].join(', ')
