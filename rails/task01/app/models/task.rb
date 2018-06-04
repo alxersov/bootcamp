babad16b7e5c36f06ea0e8da4c2e4c1399c34250
Alexei Ersov <alexei@mixbook.com>
2018-06-04 17:21:22 +0300

    Add ToDo list

---
+++ rails/task01/app/models/task.rb
@@ -0,0 +1,3 @@
+class Task < ActiveRecord::Base
+  attr_accessible :title, :is_completed
+end
---
+++ rails/task01/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Task01::Application.config.secret_token = '24b41111ad464ef9e797bafc827405a99e9109687e84a846522e7faadbdead6832434ea8f4024eedd54b22e45dd4f5a0d6f058fa5cbc9a57269c5a4920b1b0a5'
