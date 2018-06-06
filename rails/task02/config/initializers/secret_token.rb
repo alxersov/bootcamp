7d23b44ee57043b8492657415bd31d9762d08324
Alexei Ersov <alexei@mixbook.com>
2018-06-06 11:42:25 +0300

    Add new rails application

---
+++ rails/task02/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Task02::Application.config.secret_token = 'd502ac79ecff7a38546e67b45b8acdf54be25a5b8ea7f56f571494e8ddcac7751f60cfef3b32711a6358deffe754a7a0917a75aa2aa6e5dbec25dec9498527ad'
